8bd95621c292bf13c12d6616a24d58d87acc2914
q <lwzm@qq.com>
2016-11-07 14:51:54 +0800

    scripts: clean_redis_counters

---
+++ scripts/clean_redis_counters.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+redis-cli -s redis/sock eval '
+
+local ts = KEYS[1]
+
+local ops = 0
+
+for _, key in pairs(ARGV) do
+    for _, t in pairs(redis.call("hkeys", key)) do
+        if t < ts then
+            redis.call("hdel", key, t)
+            ops = ops + 1
+        end
+    end
+end
+
+return ops
+
+' 1 $(date -d '3 days ago' '+%G%m%d')  cnt_found cnt_done
